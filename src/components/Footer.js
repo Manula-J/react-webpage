80e15511be8ec7d9bcac77dde188dd4afc40186b
Manula <manula.jayabodhi@gmail.com>
2024-05-21 22:43:02 +0530

    Added a footer to the website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,11 +25,72 @@ function Footer() {
           <div className='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
-            <Link to='/'></Link>
-            
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+            <Link to='/'>LinkedIn</Link>
           </div>
         </div>
       </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='social-logo'>
+            <Link to='/' className='social-logo'>
+              TRVL <i className='fab fa-typo3' />
+            </Link>
+            <small className='website-rights'>TRVL 2020</small>
+            <div className='social-icons'>
+              <Link className='social-icon-link facebook' to='/' target='_blank' aria-label='Facebook'>
+                <i className='fab fa-facebook-f'></i>
+              </Link>
+            </div>
+            <div className='social-icons'>
+              <Link className='social-icon-link instagram' to='/' target='_blank' aria-label='Instagram'>
+                <i className='fab fa-instagram'></i>
+              </Link>
+            </div>
+            <div className='social-icons'>
+              <Link className='social-icon-link youtube' to='/' target='_blank' aria-label='Youtube'>
+                <i className='fab fa-youtube'></i>
+              </Link>
+            </div>
+            <div className='social-icons'>
+              <Link className='social-icon-link twitter' to='/' target='_blank' aria-label='Twitter'>
+                <i className='fab fa-twitter'></i>
+              </Link>
+            </div>
+            <div className='social-icons'>
+              <Link className='social-icon-link likedin' to='/' target='_blank' aria-label='LinkedIn'>
+                <i className='fab fa-linkedin'></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
